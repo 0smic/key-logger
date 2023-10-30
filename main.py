cae1c92a1861b94e937b889b9a37cae66bb87ce9
Gokul <146622216+huho-0@users.noreply.github.com>
2023-10-30 10:54:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from pynput import keyboard
+def on_press(key):
+    
+    try:
+        if key == keyboard.Key.esc:
+            print("Exiting....")
+            return False
+
+        elif key == keyboard.Key.space:
+            with open("file.txt", "a") as file:
+                file.write(" ")
+        elif key == keyboard.Key.enter:
+            with open("file.txt", "a") as file:
+                file.write("\n")
+        elif key == keyboard.Key.backspace:
+            with open("file.txt", "a") as file:
+                file.seek(file.tell() -1)
+                file.truncate()
+                
+        else:
+                
+            with open("file.txt", "a") as file:
+                file.write(f"{key.char}")
+    except AttributeError:
+        pass
+
+def detect_and_save_keystrokes():
+    print("Start typing! Press 'Esc' to quit")
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    detect_and_save_keystrokes()
+
+
